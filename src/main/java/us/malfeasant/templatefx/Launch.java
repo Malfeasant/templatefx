c3a91e11008bc6f700005082cc516edb4d5a0bd4
Malfeasant <1212508+Malfeasant@users.noreply.github.com>
2025-09-12 20:19:17 -0700

    Modularized & pom cleanup

--- src/main/java/us/malfeasant/templatefx/Launch.java
+++ src/main/java/us/malfeasant/templatefx/Launch.java
@@ -1,13 +1,15 @@
 package us.malfeasant.templatefx;
 
 /**
- * This is a hack- rather than starting the JavaFX app directly,
- * we launch this class that then calls main on the App class.
+ * This is a hack to run without modules-
+ * rather than starting the JavaFX app directly,
+ * we launch this class which then calls main() on the App class.
  * Otherwise, JavaFX subsystem doesn't get invoked properly.
- * Only needed because we're packaging into a fat jar.
+ * Only needed when we're packaging into a fat jar.
  */
 public class Launch {
     public static void main(String[] args) {
+        System.out.println("Running from Launch class");
         App.main(args);
     }
 }
